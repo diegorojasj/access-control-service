e7f6ab3e9ba5f5af545590388dbce383e97fd475
Diego Rojas Justiniano <diegorojasj222@gmail.com>
2026-03-09 23:47:47 +0000

    fix: render issue

--- frontend/src/core/AuthorizedComponent.tsx
+++ frontend/src/core/AuthorizedComponent.tsx
@@ -1,24 +1,18 @@
 import Layout from "@/core/Layout"
-import { Await, Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
 import { AppStore } from "./AppContext"
-import { useLayoutEffect } from "react"
-import { useNavigate } from "react-router-dom"
 
 const AuthorizedComponent = () => {
-    const { user, setUser } = AppStore()
-    const navigate = useNavigate()
-    useLayoutEffect(()=>{
-        if(!user){
-            navigate("/login")
-        }
-    }, [])
-    const data = {}
+    const { user } = AppStore()
+
+    if (!user) {
+        return <Navigate to="/login" replace />
+    }
+
     return (
-        <Await resolve={data}>
-            <Layout>
-                <Outlet />
-            </Layout>
-        </Await>
+        <Layout>
+            <Outlet />
+        </Layout>
     )
 }
 
