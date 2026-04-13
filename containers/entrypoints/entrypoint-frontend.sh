988f5b3e6660c55b58952a8fc4500366372ef0c0
Diego Rojas Justiniano <diegorojasj222@gmail.com>
2026-04-12 23:38:37 -0400

    feat: update nginx config and entrypoint-frontend

--- containers/entrypoints/entrypoint-frontend.sh
+++ containers/entrypoints/entrypoint-frontend.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -e
 
-envsubst '$BACKEND_URL' < /etc/nginx/templates/default.conf.template \
+RESOLVER=$(awk '/^nameserver/{print $2; exit}' /etc/resolv.conf)
+export RESOLVER
+
+envsubst '$BACKEND_URL $RESOLVER' < /etc/nginx/templates/default.conf.template \
   > /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
